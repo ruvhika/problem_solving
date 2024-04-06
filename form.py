c176eac019faa21bd898dc84fa2bc04af3c74b81
Ruchika Sonkusare <165674600+ruvhika@users.noreply.github.com>
2024-04-06 15:53:03 +0530

    Introduction form

--- form.py
+++ form.py
@@ -1,2 +1,24 @@
 # solving a task
-print(
+name=input("First Name: ")
+surname=input("Last Name:")
+email=input("email: ")
+contact=int(input("Contact: "))
+school=input("school:")
+if school=="School of engineering":
+    print("Select options:\nBtech\nBCA")
+    n=input("Enter stream:")
+    if n=="Btech" or n=="BCA":
+        print(n)
+    else:
+        print("not available")
+elif school=="School of management":
+    print("Select options:\nBBA\nMBA")
+    n=input("Enter stream:")
+    if n=="MBA" or n=="BBA":
+        print(n)
+    else:
+        print("not available")
+s=int(input("Enter semester:"))
+h=input("Course name:")
+j=input("Course code:")
+k=input("Faculty name:")
