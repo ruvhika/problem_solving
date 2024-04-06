a0fe3b8df996e68ec673a57122dfa704b3347631
SambhavJain-JS <165653608+SambhavJain-JS@users.noreply.github.com>
2024-04-06 16:08:17 +0530

    Update form.py

--- form.py
+++ form.py
@@ -22,3 +22,61 @@ s=int(input("Enter semester:"))
 h=input("Course name:")
 j=input("Course code:")
 k=input("Faculty name:")
+
+
+
+
+
+
+
+
+
+
+print("Feedback Form")
+
+a=0
+print("Are you satisfied with the overall teaching of the course:")
+print("1.Highly Satisfied")
+print("2.Moderately Satisfied")
+print("3.Not Satisfied")
+int(input("a="))
+
+if a==1:
+    o1="Highly Satisfied"
+
+
+elif a==2:
+    o1="Moderately Satisfied"
+
+elif a==3:
+    o1=".Not Satisfied"
+
+else:
+    None
+
+b=0
+print("Subject knowledge of the Course Lead :")
+print("1.Very Poor")
+print("2.Poor")
+print("3.Average")
+print("4.Good")
+print("3.Very Good")
+b=int(input("b="))
+
+if b==1:
+    o2="Very Poor"
+
+
+elif b==2:
+    o2="Poor"
+
+elif b==3:
+    o2="Average"
+
+elif b==4:
+    o2="Very Good"
+
+else:
+    None
+
+print(o2)
